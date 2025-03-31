37a7d55ad77742abae24ff9c1e692d02472e66fe
Jacobsin12 <tristanjacob755@gmail.com>
2025-03-30 23:11:10 -0600

    Implementacion de redirección segura5

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './bootstrap.css';  // Asegúrate de que la ruta sea correcta
-import Login from './screens/Login';  // Asegúrate de que este componente exista
-import Home from './screens/Home';    // Asegúrate de que este componente exista
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import './bootstrap.css';
+import Login from './screens/Login';
+import Home from './screens/Home';
 import ModalRegister from './components/ModalRegister';
 
 function App() {
   return (
-    <Router>  {/* Usamos Router para envolver nuestras rutas */}
+    <Router>
       <Routes>
-        {/* Ruta principal para el Login */}
-        <Route path="/" element={<Login />} />  {/* Cambié la ruta raíz a Login */}
-
-        {/* Ruta para el Home, que se cargará después de un login exitoso */}
+        <Route path="/" element={<Login />} />
         <Route path="home" element={<Home />} />
         <Route path="ModalRegister" element={<ModalRegister />} />
+
+        {/* Redirigir cualquier otra URL a la página principal */}
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
   );

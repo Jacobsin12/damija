38e5ddcc92d6b5df762963cf6ac2e362ce4f9685
Jacobsin12 <tristanjacob755@gmail.com>
2025-03-30 21:50:22 -0600

    Implementacion del Modal de registro e inserccion de nuevo usuario a la bd3

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -18,32 +18,33 @@ const connectToDatabase = async () => {
 
 exports.handler = async function(event, context) {
   try {
-    const { name, email, password } = JSON.parse(event.body);
-
-    if (!name || !email || !password) {
-      return {
-        statusCode: 400,
-        body: JSON.stringify({ success: false, message: 'Todos los campos son obligatorios' }),
-      };
-    }
-
     const client = await connectToDatabase();
 
-    // Insertar el nuevo usuario en la base de datos
-    const result = await client.query(
-      'INSERT INTO usuarios (nombre, correo, contrasena) VALUES ($1, $2, $3) RETURNING *',
-      [name, email, password]
-    );
+    // Leer los datos del cuerpo de la solicitud
+    const { email, password } = JSON.parse(event.body);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ success: true, user: result.rows[0] }),
-    };
+    // Consultar la base de datos para verificar la autenticación
+    const result = await client.query('SELECT * FROM usuarios WHERE correo = $1 AND contrasena = $2', [email, password]);
+
+    if (result.rows.length > 0) {
+      // Si el usuario existe y las credenciales son correctas, devolver la lista de usuarios
+      const users = await client.query('SELECT * FROM usuarios');
+      return {
+        statusCode: 200,
+        body: JSON.stringify({ success: true, users: users.rows }),
+      };
+    } else {
+      // Si no se encuentra el usuario o las credenciales son incorrectas
+      return {
+        statusCode: 401,
+        body: JSON.stringify({ success: false, message: 'Credenciales incorrectas' }),
+      };
+    }
   } catch (error) {
-    console.error('Error al registrar el usuario:', error);
+    console.error('Error al obtener los datos:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ success: false, message: 'Error al registrar el usuario' }),
+      body: JSON.stringify({ message: 'Error al obtener los datos' }),
     };
   }
 };
